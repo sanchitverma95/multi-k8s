fd1a8129ad7686fe0897d3eb04d905c4ddde98e1
Sanchit <sanchitverma95@github.com>
2020-07-31 01:46:24 +0530

    Travis file with deployment script as provider to build and push images

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+docker build -t sanchitverma/multi-client:latest -t sanchitverma/multi-client:$SHA ./client/Dockerfile ./client
+docker build -t sanchitverma/multi-server:latest -t sanchitverma/multi-server:$SHA ./server/Dockerfile ./server
+docker build -t sanchitverma/multi-worker:latest -t sanchitverma/multi-worker:$SHA ./worker/Dockerfile ./worker
+
+docker push sanchitverma/multi-client:latest
+docker push sanchitverma/multi-server:latest
+docker push sanchitverma/multi-worker:latest
+
+docker push sanchitverma/multi-client:$SHA
+docker push sanchitverma/multi-server:$SHA
+docker push sanchitverma/multi-worker:$SHA
+
+kubectl apply -f k8s 
+kubectl set image deployments/server-deployment server=sanchitverma/multi-server:$SHA
+kubectl set image deployments/client-deployment client=sanchitverma/multi-client
+:$SHA
+kubectl set image deployments/worker-deployment worker=sanchitverma/multi-worker
+:$SHA
